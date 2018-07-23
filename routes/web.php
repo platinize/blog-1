217af6ab818ea0a9008741501241be0791efc433
platinize <bonch_igor@mail.ru>
2018-07-23 09:05:59 +0300

    Add roles

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,15 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/main', 'PostController@index')->name('main');
 Route::get('show/{slug}', 'PostController@show')->name('post.show');
 
-Route::get('admin', 'AdminController@index')->name('admin.panel');
-Route::get('create', 'PostController@create')->name('post.create');
-Route::post('create', 'PostController@store')->name('post.store');
-Route::get('edit/{slug}', 'PostController@edit')->name('post.edit');
-Route::post('edit/{slug}', 'PostController@update')->name('post.update');
-Route::get('delete/{id}', 'PostController@destroy')->name('post.destroy');
+Route::group(['middleware' => ['role:super-admin']], function () {
+    Route::get('admin', 'AdminController@index')->name('admin.panel');
+    Route::get('create', 'PostController@create')->name('post.create');
+    Route::post('create', 'PostController@store')->name('post.store');
+    Route::get('edit/{slug}', 'PostController@edit')->name('post.edit');
+    Route::post('edit/{slug}', 'PostController@update')->name('post.update');
+    Route::get('delete/{id}', 'PostController@destroy')->name('post.destroy');
+});
+
 
 //Ajax
 Route::get('like/{postId}', 'Ajax\LikeController@createOrDestroy')->name('like');
